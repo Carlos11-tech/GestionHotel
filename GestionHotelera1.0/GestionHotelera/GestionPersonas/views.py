8a01fafc39e2c603f1a65aa1802beda5edf355a1
Cristopher David Chingo Rivera <166523123+DavidCh02@users.noreply.github.com>
2024-06-03 17:38:27 -0500

    Add views de la app

--- GestionHotelera1.0/GestionHotelera/GestionPersonas/views.py
+++ GestionHotelera1.0/GestionHotelera/GestionPersonas/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PersonaForm, ClienteForm, PersonalForm, ReservaForm, CheckOutForm
+from .models import Persona, Cliente, Personal, Reserva, CheckOut
 
-# Create your views here.
+
+def crear_cliente(request):
+    if request.method == "POST":
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_clientes')
+    else:
+        form = ClienteForm()
+    return render(request, 'hotel/cliente_form.html', {'form': form})
+
+
+def editar_cliente(request, pk):
+    cliente = get_object_or_404(Cliente, pk=pk)
+    if request.method == "POST":
+        form = ClienteForm(request.POST, instance=cliente)
+        if form.is_valid():
+            form.save()
+            return redirect('detalle_cliente', pk=cliente.pk)
+    else:
+        form = ClienteForm(instance=cliente)
+    return render(request, 'hotel/cliente_form.html', {'form': form})
+
